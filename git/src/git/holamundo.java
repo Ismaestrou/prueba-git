6478cf2cf76d1f5377640b10b1aa581c68f0c0f1
Ismael Busto <ismael.busto@opendeusto.es>
2024-09-20 08:43:02 +0200

    poner holamundo.java

--- git/src/git/holamundo.java
+++ git/src/git/holamundo.java
@@ -5,5 +5,6 @@ public class holamundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Adios mundo");
+		System.out.println("Hola mundo");
 	}
 }
