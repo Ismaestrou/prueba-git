372787d6ce937b3dc33f6d4b999d77d315bff4d5
Ismael Busto <ismael.busto@opendeusto.es>
2024-09-20 08:42:44 +0200

    quitar holamundo.java

--- git/src/git/holamundo.java
+++ git/src/git/holamundo.java
@@ -5,6 +5,5 @@ public class holamundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Adios mundo");
-		System.out.println("Hola mundo");
 	}
 }
